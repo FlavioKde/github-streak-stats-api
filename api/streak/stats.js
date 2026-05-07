897f6918cb6da03af5f7e2b7db1caae8795e2280
FlavioKde <flaviodavirro@gmail.com>
2026-05-07 15:34:24 +0200

    feat(stats): add translator import and function calculateStreak

--- api/streak/stats.js
+++ api/streak/stats.js
@@ -2,9 +2,17 @@ import { fetchUserContributions } from '../../lib/github/githubClient.js';
 import { ValidationError } from '../../lib/shared/errors/index.js';
 import { formatJsonResponse } from '../../lib/render/formatJsonResponse.js';
 import { handleJsonError } from '../../lib/http/handleJsonError.js';
-import { getCachedContributions } from '../../lib/cache/contributionsCache.js'; 
+import { getCachedContributions } from '../../lib/cache/contributionsCache.js';
+import { calculateStreak } from '../../lib/streak/calculateStreak.js';
+import { createTranslator } from '../../lib/i18n/index.js'; 
+
+
 
 export default async function handler(req, res) {
+
+    const lang = req.query.lang;
+    const t = createTranslator(lang);
+
   try {
     const { user } = req.query;
 
@@ -14,10 +22,13 @@ export default async function handler(req, res) {
     }
    
     const contributions = await getCachedContributions(fetchUserContributions, user);
-    const streakData = formatJsonResponse(contributions);
+    //const streakData = formatJsonResponse(contributions);
+    const streakData = calculateStreak(contributions);
+    const formattedResponse = formatJsonResponse(streakData, t);
 
    res.setHeader('Cache-Control', 'public, max-age=43200, s-maxage=43200, stale-while-revalidate=3600');
-   res.status(200).json(streakData);
+   //res.status(200).json(streakData);
+    res.status(200).json(formattedResponse);
     
   } catch (error) {
    
