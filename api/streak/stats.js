4e090b0264685afe7e21a4375c24c8a5649e2aa2
FlavioKde <flaviodavirro@gmail.com>
2026-05-16 18:26:43 +0200

    chore(stats): remove i18n for jsonEndpoint to keep it a  pure API

--- api/streak/stats.js
+++ api/streak/stats.js
@@ -4,14 +4,12 @@ import { formatJsonResponse } from '../../lib/render/formatJsonResponse.js';
 import { handleJsonError } from '../../lib/http/handleJsonError.js';
 import { getCachedContributions } from '../../lib/cache/contributionsCache.js';
 import { calculateStreak } from '../../lib/streak/calculateStreak.js';
-import { createTranslator } from '../../lib/i18n/index.js'; 
+
 
 
 
 export default async function handler(req, res) {
 
-    const lang = req.query.lang;
-    const t = createTranslator(lang);
 
   try {
     const { user } = req.query;
@@ -24,7 +22,7 @@ export default async function handler(req, res) {
     const contributions = await getCachedContributions(fetchUserContributions, user);
 
     const streakData = calculateStreak(contributions);
-    const formattedResponse = formatJsonResponse(streakData, t);
+    const formattedResponse = formatJsonResponse(streakData);
 
    res.setHeader('Cache-Control', 'public, max-age=43200, s-maxage=43200, stale-while-revalidate=3600');
    
@@ -32,7 +30,7 @@ export default async function handler(req, res) {
     
   } catch (error) {
    
-    handleJsonError(res, error, lang);
+    handleJsonError(res, error);
 
   }  
 
