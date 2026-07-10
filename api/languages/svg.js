bfb23a5caaca0869c42bbd5a374e7d5d7a3cd4c3
FlavioKde <flaviodavirro@gmail.com>
2026-07-10 23:01:05 +0200

    feat(svg): add SVG output for languages stats

---
+++ api/languages/svg.js
@@ -0,0 +1,39 @@
+import { fetchUserLanguages } from "../../lib/github/githubClient";
+import { ValidationError } from "../../lib/shared/errors/ValidationError.js";
+import { renderLanguagesSvg } from "../../lib/render/renderLanguagesSvg.js";
+import { sendSvgResponse } from "../../lib/render/sendSvgResponse.js";
+import { handleSvgError } from "../../lib/http/handleSvgError.js";
+import { getCachedLanguages } from "../../lib/cache/languagesCache.js";
+import { getTheme } from "../../lib/themes/themes.js";
+import { createTranslator } from "../../lib/i18n/index.js";
+
+
+export default async function handler(req, res) {
+
+  const lang = req.query.lang;
+  const t = createTranslator(lang);
+
+    try {
+        const { user, theme = "dark" } = req.query;
+        if (!user) {
+            throw new ValidationError("Missing required query parameter: user");
+        }
+
+
+        const selectedTheme = getTheme(theme) || getTheme("dark");
+
+        const languagesData = await getCachedLanguages(fetchUserLanguages, user);   
+
+        const svg = renderLanguagesSvg(languagesData, selectedTheme, t);
+
+        sendSvgResponse({
+            res,
+            status: 200,
+            svgString: svg
+        });
+
+    } catch (error) {
+        handleSvgError(res, error, lang);
+    }
+
+}
